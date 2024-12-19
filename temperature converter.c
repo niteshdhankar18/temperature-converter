8345f810d2bdfd145374478a02b38ee7aadb3992
niteshdhankar18 <niteshdhankar06@gmail.com>
2024-12-19 18:39:37 +0530

    Update temperature converter.c

--- temperature converter.c
+++ temperature converter.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-void celciustofahrenhiet(){
-  float farnhiet, celcius;
-    printf("enter temp in celciust ");
+void celciustofahrenheit(){
+  float fahrenheit, celcius;
+    printf("enter temp in celcius ");
     scanf("%f",&celcius);
-    farnhiet = (9.0*celcius)/5.0+32.0;
-    printf("celcius to fahrenhiet for %.2f is %.2f\n",celcius,farnhiet);
+    fahrenheit = (9.0*celcius)/5.0+32.0;
+    printf("celcius to fahrenheit for %.2f is %.2f\n",celcius,fahrenheit);
 }
 // void fahrenhiettocelcius(float);
-void fahrenhiettocelcius(){
-float farnhiet, celcius;
-    printf("enter temp in fahrenhiet ");
-    scanf("%f",&farnhiet);
-    celcius = (farnhiet-32.0)*5.0/9.0;
-    printf("fahrenhiet to celcius for %.2f is %.2f\n",farnhiet , celcius);
+void fahrenheittocelcius(){
+float fahrenheit, celcius;
+    printf("enter temp in fahrenheit ");
+    scanf("%f",&fahrenheit);
+    celcius = (fahrenheit-32.0)*5.0/9.0;
+    printf("fahrenheit to celcius for %.2f is %.2f\n",fahrenheit, celcius);
 }
 // void  celciustofahrenhiet(float);
 
 int main() {
     int choice;
-    printf("1. Convert Celsius to Fahrenheit\n");
-    printf("1. Convert Fahrenheit to Celsius\n");
+    printf("1. Convert Celsius to fahrenheit\n");
+    printf("1. Convert fahrenheit to Celsius\n");
     printf("enter choice 1 or 2 :");
     scanf("%d",&choice);
     switch (choice)
     {
     case 1:
-        celciustofahrenhiet();
+        celciustofahrenheit();
         break;
     case 2:
-    fahrenhiettocelcius();
+   fahrenheittocelcius();
     break;
     default:
         printf("invalid choice!");
